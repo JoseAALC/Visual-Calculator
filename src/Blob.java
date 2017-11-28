d6ede8f962cd52dd5c5f381eefd51d56dca9b777
Cristiano <cristianobarbosa1996@gmail.com>
2017-11-28 19:30:22 +0000

    flood fill started

--- src/Blob.java
+++ src/Blob.java
@@ -1,4 +1,13 @@
 
 public class Blob {
-
+	int content[][];
+	int p1x, p1y, p2x,p2y;
+	public Blob(int content[][], int p1x, int p1y, int p2x, int p2y){
+		this.content= content;
+		this.p1x=p1x;
+		this.p1y=p1y;
+		this.p2x=p2x;
+		this.p2y=p2y;
+	}
+	
 }
--- src/Processor.java
+++ src/Processor.java
@@ -38,6 +38,17 @@ public class Processor {
 	
 	
 	public ArrayList<Blob> floodFill(int segmentedImage[][]) {
+		int componentCount=0;
+		int componentMarker=2;
+		
+		for(int i=0; i<segmentedImage.length; i++)
+			for(int j=0; j<segmentedImage[i].length; j++){
+				
+				if(segmentedImage[i][j]==1){ //encontrou um, agora faz bloodfill
+					
+				}
+			}
+		
 		return null;
 		
 	}
